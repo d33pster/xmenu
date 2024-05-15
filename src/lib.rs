38f431bd883bcf4c66231c05beeadec9f44cfd04
d33pster <d33pster@icloud.com>
2024-05-16 02:41:44 +0530

    fix issues, add demo

--- src/lib.rs
+++ src/lib.rs
@@ -11,6 +11,20 @@ use crossterm::{
 
 use std::io::{self, Write};
 
+use colorized::*;
+
+pub enum Colour {
+    Blue,
+    Black,
+    Cyan,
+    Green,
+    Magenta,
+    Red,
+    White,
+    Yellow,
+    Default,
+}
+
 pub struct Xmenu{
     options: Vec<String>,
 }
@@ -37,7 +51,19 @@ impl Xmenu {
         self.options = new;
     }
 
-    pub fn run(&mut self) -> String {
+    pub fn run(&mut self, c: Colour) -> String {
+        let col = match c {
+            Colour::Black => "black",
+            Colour::Blue => "blue",
+            Colour::Cyan => "cyan",
+            Colour::Green => "green",
+            Colour::Magenta => "magenta",
+            Colour::Red => "red",
+            Colour::White => "white",
+            Colour::Yellow => "yellow",
+            Colour::Default => "none",
+        };
+
         let mut stdout = io::stdout();
 
         // set terminal to raw mode
@@ -55,7 +81,7 @@ impl Xmenu {
         let menu_height = menu_items.len() as u16;
 
         // Draw the initial menu
-        Xmenu::draw_menu(&menu_items, selected_index, orig_x, orig_y);
+        Xmenu::draw_menu(&menu_items, selected_index, orig_x.clone(), orig_y.clone(), col);
 
         loop {
             if let Event::Key(key_event) = event::read().expect("Failed to read key") {
@@ -81,7 +107,7 @@ impl Xmenu {
                 }
 
                 // redraw the menu
-                Xmenu::draw_menu(&menu_items, selected_index, orig_x, orig_y - menu_height);
+                Xmenu::draw_menu(&menu_items, selected_index, orig_x.clone(), orig_y.clone() - menu_height, col);
             }
         }
 
@@ -92,19 +118,65 @@ impl Xmenu {
         return self.options[selected_index].clone();
     }
 
-    fn draw_menu(menu_items: &Vec<String>, selected_index: usize, x: u16, y: u16) {
-        let mut stdout = io::stdout();
-        for (i, item) in menu_items.iter().enumerate() {
-            let msg = "Failed to insert menu item ".to_string() + &i.to_string();
-            execute!(stdout, cursor::MoveTo(x, y+i as u16)).expect(&msg);
+    // fn draw_menu(menu_items: &Vec<String>, selected_index: usize, x: u16, y: u16, c: &str) {
+    //     let mut stdout = io::stdout();
+    //     for (i, item) in menu_items.iter().enumerate() {
+    //         let msg = "Failed to insert menu item ".to_string() + &i.to_string();
+    //         execute!(stdout, cursor::MoveTo(x, y+i as u16)).expect(&msg);
 
+    //         if i == selected_index {
+    //             if c == "red" {
+    //                 writeln!(stdout, "> {}", item.color(Colors::RedFg)).expect("Failed to update menu item.");
+    //             } else if c == "yellow" {
+    //                 writeln!(stdout, "> {}", item.color(Colors::YellowFg)).expect("Failed to update menu item.");
+    //             } else if c == "green" {
+    //                 writeln!(stdout, "> {}", item.color(Colors::GreenFg)).expect("Failed to update menu item.");
+    //             } else if c == "cyan" {
+    //                 writeln!(stdout, "> {}", item.color(Colors::CyanFg)).expect("Failed to update menu item.");
+    //             } else if c == "magenta" {
+    //                 writeln!(stdout, "> {}", item.color(Colors::MagentaFg)).expect("Failed to update menu item.");
+    //             } else if c == "blue" {
+    //                 writeln!(stdout, "> {}", item.color(Colors::BlueFg)).expect("Failed to update menu item.");
+    //             } else if c == "black" {
+    //                 writeln!(stdout, "> {}", item.color(Colors::BlackFg)).expect("Failed to update menu item.");
+    //             } else if c == "white" {
+    //                 writeln!(stdout, "> {}", item.color(Colors::WhiteFg)).expect("Failed to update menu item.");
+    //             } else {
+    //                 writeln!(stdout, "> {}", item).expect("Failed to update menu item.");
+    //             }
+    //         } else {
+    //             writeln!(stdout, "  {}", item).expect("Failed to update menu item.");
+    //         }
+    //     }
+
+    //     stdout.flush().expect("Error flushing output");
+    // }
+    fn draw_menu(menu_items: &Vec<String>, selected_index: usize, x: u16, mut y: u16, c: &str) {
+        let mut stdout = io::stdout();
+        execute!(stdout, cursor::MoveTo(x, y)).expect("Failed to move cursor");
+    
+        for (i, item) in menu_items.iter().enumerate() {
             if i == selected_index {
-                writeln!(stdout, "> {}", item).expect("Failed to update menu item.");
+                let colored_item = match c {
+                    "red" => item.color(Colors::RedFg),
+                    "yellow" => item.color(Colors::YellowFg),
+                    "green" => item.color(Colors::GreenFg),
+                    "cyan" => item.color(Colors::CyanFg),
+                    "magenta" => item.color(Colors::MagentaFg),
+                    "blue" => item.color(Colors::BlueFg),
+                    "black" => item.color(Colors::BlackFg),
+                    "white" => item.color(Colors::WhiteFg),
+                    _ => item.to_string(), // Default color
+                };
+                writeln!(stdout, "> {}", colored_item).expect("Failed to update menu item.");
             } else {
                 writeln!(stdout, "  {}", item).expect("Failed to update menu item.");
             }
+            y += 1; // Move cursor to the next line
+            execute!(stdout, cursor::MoveTo(x, y)).expect("Failed to move cursor");
         }
-
+    
         stdout.flush().expect("Error flushing output");
     }
+    
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,101 @@
-use xmenu::Xmenu;
+use std::io::Write;
+
+use xmenu::{Xmenu, Colour};
 
 fn main() {
     let mut xm = Xmenu::new();
-    xm.add("https");
-    xm.add("ssh");
-    let result = xm.run();
-    println!("{}", result);
+    println!("Use Up and Down arrows to navigate. Press Enter to choose. Press Esc to Exit.");
+    xm.add("Show demo without colour.");
+    xm.add("Show demo.");
+    xm.add("Fix Windows-Terminal-Colour Problem.");
+    let result = xm.run(Colour::Default);
+    
+    if result == "Abort".to_string() {
+        std::process::exit(0);
+    } else if result == "Show demo without colour.".to_string() {
+        println!("\nDemo without colour");
+        let mut xm = Xmenu::new();
+        xm.add("https");
+        xm.add("ssh");
+        let demo_res = xm.run(Colour::Default);
+        println!("You chose: {}", demo_res);
+    } else if result == "Show demo.".to_string() {
+        println!("\nDemo with color:");
+        let mut xm = Xmenu::new();
+        xm.add("https");
+        xm.add("ssh");
+        let demo_res = xm.run(Colour::Blue);
+        println!("You chose: {}", demo_res);
+    } else if result == "Fix Windows-Terminal-Colour Problem.".to_string() {
+        let os = std::env::consts::OS;
+
+        if os!="windows" {
+            eprintln!("This feature is only for Windows.");
+            std::process::exit(0);
+        }else {
+            match make_dot_bat_file() {
+                Ok(()) => {println!("Wrote -> fix-terminal.bat")},
+                Err(e) => {eprintln!("Error: {}", e.to_string())},
+            }
+
+            let _output = std::process::Command::new("cmd")
+                .args(&["/C", "fix-terminal.bat"])
+                .output()
+                .expect("Error running fix-terminal.bat. Double-Click on it. Its in the current working directory.");
+
+            println!("Fixed");
+        }
+    }
+}
+
+fn make_dot_bat_file() -> std::io::Result<()> {
+    let filepath = "fix-terminal.bat";
+    let mut file = std::fs::File::create(filepath)?;
+
+    file.write_all(r###"@echo off
+:: BatchGotAdmin
+:--------------------------
+@Rem --> Check for Permission
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+
+@Rem if error flag set, we do not have admin.
+if %errorlevel% NEQ 0 (
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
+    pushd "%CD%"
+    CD /D "%~dp0"
+:-----------------------------------
+
+@Rem check registry for ansi colour code
+echo [REGISTRY CHECK]
+reg query HKCU\Console\ | find /I "VirtualTerminalLevel" > nul
+if %errorlevel% NEQ 0 (
+    @Rem ANSI settings not found.
+    reg add HKCU\Console /v VirtualTerminalLevel /t REG_DWORD /d 1
+    echo added registry for ANSI escape sequence
+) else (
+    echo found existing registry for ANSI escape sequence. Skipping..
+)
+
+echo.
+echo FIXED TERMINAL COLOUR PROBLEM
+setlocal
+
+set /p choke=
+
+endlocal"###.as_bytes())?;
+
+    file.flush()?;
+
+    Ok(())
 }
